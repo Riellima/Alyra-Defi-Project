5018e6f58767bd10e364b63bfa732522d653e04d
CleiK <29407538+CleiK@users.noreply.github.com>
2022-05-02 14:29:10 +0200

    token and staking constructors, dependencies

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -1,6 +1,53 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.13;
 
+
+import "../node_modules/@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "./Token.sol";
+
 contract Staking {
+
+  AggregatorV3Interface internal _ethUsdPriceFeed; 
+  UniFiToken internal _unifiToken;
   
+  constructor(address ethUsdPriceFeedAddress_)  {
+    _ethUsdPriceFeed = AggregatorV3Interface(ethUsdPriceFeedAddress_);
+    _unifiToken = new UniFiToken(0);
+  }
+
+  /**
+   * Returns the latest price
+   */
+  function getLatestPrice() public view returns (int) {
+    (
+      /*uint80 roundID*/,
+      int price,
+      /*uint startedAt*/,
+      /*uint timeStamp*/,
+      /*uint80 answeredInRound*/
+    ) = _ethUsdPriceFeed.latestRoundData();
+      return price;
+  }
+
+
+  function deposit() public {
+
+  }
+
+  function withdraw() public {
+
+  }
+
+  function claim() public {
+
+  }
+
+  function getTotalValueLocked() public view {
+
+  }
+
+  function getUserValueLocked() public view {
+
+  }
+
 }
---
+++ contracts/Token.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
+
+import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
+
+contract UniFiToken is ERC20, Ownable {
+
+  constructor(uint256 initialSupply_) ERC20("UniFi", "UNF") {
+    _mint(msg.sender, initialSupply_);    
+  }
+
+  function mintTo(address to_, uint256 amount_) public onlyOwner {
+    _mint(to_, amount_);
+  }
+}
